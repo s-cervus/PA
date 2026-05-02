42361c2c39c4af596f2016d06ace7eb23ad02b12
User <User@SHARD>
2026-05-01 21:24:43 -0600

    01052026

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,7 +45,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(332, 80);
+            this.label1.Location = new System.Drawing.Point(343, 91);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 1;
