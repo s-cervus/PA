1a5ed5dbf3774e20c49ba9baf60277160fd8870c
Fernando Saavedra <fernandosaavedracf@outlook.com>
2026-05-23 16:31:40 -0600

    4:30pm del 23-05

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnEntrada = new System.Windows.Forms.Button();
+            this.btnSalida = new System.Windows.Forms.Button();
+            this.btnSucursales = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEntrada
+            // 
+            this.btnEntrada.Location = new System.Drawing.Point(130, 139);
+            this.btnEntrada.Name = "btnEntrada";
+            this.btnEntrada.Size = new System.Drawing.Size(134, 54);
+            this.btnEntrada.TabIndex = 0;
+            this.btnEntrada.Text = "Entrada";
+            this.btnEntrada.UseVisualStyleBackColor = true;
+            // 
+            // btnSalida
+            // 
+            this.btnSalida.Location = new System.Drawing.Point(130, 244);
+            this.btnSalida.Name = "btnSalida";
+            this.btnSalida.Size = new System.Drawing.Size(134, 54);
+            this.btnSalida.TabIndex = 1;
+            this.btnSalida.Text = "Salida";
+            this.btnSalida.UseVisualStyleBackColor = true;
+            this.btnSalida.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSucursales
+            // 
+            this.btnSucursales.Location = new System.Drawing.Point(130, 339);
+            this.btnSucursales.Name = "btnSucursales";
+            this.btnSucursales.Size = new System.Drawing.Size(134, 54);
+            this.btnSucursales.TabIndex = 2;
+            this.btnSucursales.Text = "Sucursales";
+            this.btnSucursales.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Reciclaje tecnologico";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(432, 496);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSucursales);
+            this.Controls.Add(this.btnSalida);
+            this.Controls.Add(this.btnEntrada);
+            this.Name = "Form3";
             this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEntrada;
+        private System.Windows.Forms.Button btnSalida;
+        private System.Windows.Forms.Button btnSucursales;
+        private System.Windows.Forms.Label label1;
     }
 }
