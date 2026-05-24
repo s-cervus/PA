7bdfe295ed5eaf96e5b91d2611ca1725e6f69686
Fernando Saavedra <fernandosaavedracf@outlook.com>
2026-05-24 14:10:23 -0600

    2:09 pm 24/05/26

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Form4());
         }
     }
 }
