b4757aeef4445b0fbe99c5044152da418ebbc9cd
Monserrat Correa Calvillo <Correacalvillo8@gmail.com>
2026-05-25 21:42:37 -0600

    25/05/2026 9:34p.m

--- Form5.cs
+++ Form5.cs
@@ -16,5 +16,15 @@ namespace PA
         {
             InitializeComponent();
         }
+
+        private void lblEstado_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
