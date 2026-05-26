f491aa5c82d6a8293ae1e81cb021142ff4ee35ca
CECyT13 <CECyT13@E18>
2026-05-26 13:27:52 -0600

    1327260052026

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -217,9 +217,9 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-19, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(-92, 6);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(891, 589);
+            this.pictureBox1.Size = new System.Drawing.Size(998, 589);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
